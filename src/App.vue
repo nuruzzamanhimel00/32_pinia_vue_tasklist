0b52a5cfb9b57fbc013352543f87a3884da08b0c
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2023-01-20 23:37:07 +0600

    done

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
         <!-- .header -->
         <header>
           <img src="../public/pinia.svg" alt="pinia image">
-          <h2>{{ taskStore.name }}</h2>
+          <h2>{{ name }}</h2>
         </header>
 
          <!-- loading -->
-        <div class="loading" v-if="taskStore.loading">Loading tasks...</div>
+        <div class="loading" v-if="loading">Loading tasks...</div>
 
 
          <!-- new task form -->
@@ -17,21 +17,21 @@
           <task-form />
         </div>
         <nav class="text-center my-4">
-          <button class="btn btn-success mr-2" @click.prevent="filter = 'all' ">All tasks ({{ taskStore.totalCount }}) </button>
-          <button class="btn btn-danger mr-2" @click.prevent="filter = 'favs' ">Favs tasks({{ taskStore.favCount }})</button>
+          <button class="btn btn-success mr-2" @click.prevent="filter = 'all' ">All tasks ({{ totalCount }}) </button>
+          <button class="btn btn-danger mr-2" @click.prevent="filter = 'favs' ">Favs tasks({{ favCount }})</button>
           <button @click.prevent="taskStore.$reset()" class="btn btn-warning" >Reset</button>
         </nav>
         <!-- task list.. -->
         <div class="task-list" v-if="filter == 'all' ">
-          <h1>All tasks({{ taskStore.totalCount }}):-</h1>
-          <div v-for="task in taskStore.tasks" :key="task.id">
+          <h1>All tasks({{ totalCount }}):-</h1>
+          <div v-for="task in tasks" :key="task.id">
             <!-- <p>{{ task.title }}</p> -->
             <task-details :task="task" />
           </div>
         </div>
         <div class="task-list" v-if="filter == 'favs' ">
-          <h1>Fav tasks ({{ taskStore.favCount }}):-</h1>
-          <div v-for="task in taskStore.favs" :key="task.id">
+          <h1>Fav tasks ({{ favCount }}):-</h1>
+          <div v-for="task in favs" :key="task.id">
             <!-- <p>{{ task.title }}</p> -->
             <task-details :task="task" />
           </div>
@@ -48,6 +48,7 @@
   import TaskDetails from './components/TaskDetails.vue'
   import TaskForm from './components/TaskForm.vue'
   import { ref } from 'vue';
+import { storeToRefs } from 'pinia';
 
   export default {
     setup(){
@@ -56,9 +57,11 @@
         // fetch tasks
       taskStore.getTasks()
 
+      const {tasks, loading, name, favs, favCount, totalCount} = storeToRefs(taskStore);
+
       const filter = ref('all');
 
-      return {taskStore, filter};
+      return {taskStore, filter, tasks, loading, name, favs, favCount, totalCount};
     },
     name:'App',
     components:{
